c10a9675d3b55e87748b6cdb0ce7296b7d04f37c
Saradha Boothalingham <saradha.b@ebmpapst.local>
2022-12-14 19:37:35 +0530

    initial commit

---
+++ db.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const dbUser = process.env.MONGODB_NAME;
+const dbpasswd = process.env.MONGODB_PASSWD;
+const clusterAddress = process.env.MONGODB_URL; 
+
+// Connection URL
+const url = 'mongodb+srv://${dbUser}:${dbpasswd}@${clusterAddress}/?retryWrites=true&w=majority';
+
+// Database Name
+const dbName = 'Cluster0';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
